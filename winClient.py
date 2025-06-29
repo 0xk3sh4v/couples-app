25a877e6f0568fe73aad4dead23cadf29494df86
Keshav <115079501+0xk3sh4v@users.noreply.github.com>
2025-06-29 16:00:08 +0530

    Update winClient.py

--- winClient.py
+++ winClient.py
@@ -1,9 +1,10 @@
 import websocket
 from win10toast import ToastNotifier
 from plyer import notification
+import time
 
 # add to startup folder to run on boot
-# pyinstaller --onefile --noconsole --hidden-import=plyer.platforms.win.notification winClient.py
+# pyinstaller --onefile --noconsole --hidden-import=plyer.platforms.win.notification main.py
 
 def on_message(ws, message):
     notification.notify(
@@ -21,13 +22,13 @@ def on_error(ws, error):
 def on_close(ws, close_status_code, close_msg):
     print(f"[✖] Disconnected. Code: {close_status_code}, Reason: {close_msg}")
 
-if __name__ == "__main__":
-    ws = websocket.WebSocketApp(
-        "ws://melo.wisp.uno:12227/",
-        on_open=on_open,
-        on_message=on_message,
-        on_error=on_error,
-        on_close=on_close
-    )
+ws = websocket.WebSocketApp(
+    "ws://ip:port/",
+    on_open=on_open,
+    on_message=on_message,
+    on_error=on_error,
+    on_close=on_close
+)
 
-    ws.run_forever()
+while True:
+    ws.run_forever(ping_interval=30, ping_timeout=10)
